11b0142545aa88317e4f15d6e10366c1b5a7b399
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-03-03 11:04:47 +0500

    add some basic

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+// var =>functional scope;
+// let => block scope
+// const =>block scope
+// function sayHello() {
+//   for (var i = 0; i < 5; i++) {
+//     console.log(i);
+//   }
+//   console.log(i);
+// }
+// sayHello();
+// THIS keywork refrence of the current object or a refrence to object
+// jo object hai us ka refrence
+// const person = {
+//   walk() {
+//     setTimeout(function () {
+//       console.log(this);
+//     }, 1000);
+//   },
+// };
+// person.walk();
+
+// destructing
+// const obj1 = {
+//   first: "ali",
+//   last: "zaidi",
+//   degree: "master",
+// };
+// const { first: ft, last, degree } = obj1;
+// console.log(ft, ";;;;");
+// spread operator combining array
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+// const newarr = [...arr1, "ali", ...arr2];
+// console.log(newarr);
+// spread operator combining object
+const obj1 = {
+  first: "ali",
+};
+const obj2 = {
+  last: "raza",
+};
+const newObj = { ...obj1, ...obj2, location: "australi" };
+console.log(newObj);
